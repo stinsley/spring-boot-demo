796fae7ccfe6623b07deff0867f379f73ced7870
stinsley <stephentinsley00@gmail.com>
2020-05-23 12:58:39 -0600

    refactored name

--- src/main/java/com/tinsley/demo/JavaFunDemoApplication.java
+++ src/main/java/com/tinsley/demo/JavaFunDemoApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FibronacciDemoApplication {
+public class JavaFunDemoApplication {
 
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(FibronacciDemoApplication.class, args);
+		SpringApplication.run(JavaFunDemoApplication.class, args);
 	}
 
 }
