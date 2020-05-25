bbc7c71cec331d6b2b4cfca7e3158be43755f914
stinsley <stephentinsley00@gmail.com>
2020-05-25 10:04:11 -0600

    imports optimized

--- src/main/java/com/tinsley/demo/SpringDemoApplication.java
+++ src/main/java/com/tinsley/demo/SpringDemoApplication.java
@@ -1,11 +1,9 @@
 package com.tinsley.demo;
 
 import io.micrometer.core.instrument.MeterRegistry;
-import io.prometheus.client.CollectorRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
